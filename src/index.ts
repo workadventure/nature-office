049c62cf4bfe6fffd120aa65b0e873f53ce77998
ValdoTR <vteixeiraromao@gmail.com>
2022-01-31 17:35:49 +0000

    Deploying to gh-pages from master @ db6079c2dcb32aa2e31b5a6e05d2f794293af228 🚀

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
-// The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
-bootstrapExtra().catch(e => console.error(e));
-
 console.log('Script started successfully');
 
 // Waiting for the API to be ready
